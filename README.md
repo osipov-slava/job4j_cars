5fc5b5e99a595afdb507f46feab2e31a4964f4fe
Slava Osipov <skybright@mail.ru>
2024-03-25 18:36:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# job4j_cars
+## Project on course job4j.ru
+### Description
+* In project implement cars shop.
+
+### Author
+Viacheslav Osipov  
+[slavaosipov1199@gmail.com](mailto:slavaosipov1199@gmail.com)  
+[LinkedIn](https://www.linkedin.com/in/viacheslav-osipov-67806ab3/)
---
+++ src/main/java/ru/job4j/CarsApplication.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CarsApplication.class, args);
+    }
+}
